3cf65b99d7e510490d67911ebccba2acb03af918
imgurbot12 <imgurbot12@gmail.com>
2020-07-05 00:17:01 -0700

    fix: fixed url link in setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     author='imgurbot12',
     author_email='imgurbot12@gmail.com',
-    url='https://github.com/wcember/pypub',
+    url='https://github.com/imgurbot12/pydns',
     license='MIT',
     description="a small dns packet library designed for both parsing/creation"
 )
